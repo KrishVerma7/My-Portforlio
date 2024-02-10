5861455d19f3c036101e09262a4a9642e5372848
Krish Verma <dhurandhar0987@gmail.com>
2024-02-10 19:04:03 +0530

    fixed color and about section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/Untitled design compresses.png";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Profile photo";
 
 const Home = ({ name, title }) => {
   return (
@@ -30,7 +30,7 @@ const Home = ({ name, title }) => {
       <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
-        <h2>{title}</h2>
+        <h2 style={{ color: "white" }}>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
